cdf481df6d0a12637dfd356099abb2eb5ddab20e
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-07-26 18:56:09 +0530

    json-server add

--- index.js
+++ index.js
@@ -1,66 +1,87 @@
-var fs = require("fs");
-var data = fs.readFileSync("db.json");
-var elements = JSON.parse(data);
-const express = require("express");
-const app = express();
-const cors = require("cors");
+// var fs = require("fs");
+// var data = fs.readFileSync("db.json");
+// console.log("data", data);
 
-app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
+// var elements = JSON.parse(data);
+// console.log("elements", elements);
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
 
-app.use(express.static("public"));
-app.use(cors());
-app.get("/Shopping", Shopping);
-function Shopping(request, response) {
-  response.send(elements.Shopping);
-}
-app.post("/Shopping", Shopping);
-function Shopping(request, response) {
-  response.send(elements.Shopping);
-}
+// app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
 
-// app.put("/Shopping/:id", Shopping);
-
+// app.use(express.static("public"));
+// app.use(cors());
+// app.get("/Shopping", Shopping);
 // function Shopping(request, response) {
-//   const accountId = request.params.id;
-//   response.send(elements.Shopping[accountId]);
+//   response.send(elements.Shopping);
+// }
+// app.post("/Shopping", Shopping);
+// function Shopping(request, response) {
+//   response.send(elements.Shopping);
 // }
 
-app.post("/Shopping/card", card);
+// app.get("/Shopping/:id", Shopping);
 
-function card(request, response) {
-  response.send(elements.card);
-}
-app.get("/Shopping/card", card);
-function card(request, response) {
-  response.send(elements.card);
-}
+// async function Shopping(request, response) {
+//   const _id = request.params.id;
+//   console.log(_id, "id");
+//   const Shoppingdata = await Shopping.findById(_id);
+//   response.send(Shoppingdata);
+// }
 
-// app.patch("/Shopping/card/:id", card);
+// app.post("/Shopping/card", card);
+
 // function card(request, response) {
 //   response.send(elements.card);
 // }
-app.post("/login", Login);
-function Login(request, response) {
-  response.send(elements.login);
-}
-app.get("/login", Login);
-function Login(request, response) {
-  response.send(elements.login);
-}
-app.post("/register", Register);
-function Register(request, response) {
-  response.send(elements.register);
-}
-app.get("/register", Register);
-function Register(request, response) {
-  response.send(elements.register);
-}
+// app.get("/Shopping/card", card);
+// function card(request, response) {
+//   response.send(elements.card);
+// }
 
-app.post("/payment", Payment);
-function Payment(request, response) {
-  response.send(elements.payment);
-}
-app.get("/payment", Payment);
-function Payment(request, response) {
-  response.send(elements.payment);
-}
+// // app.patch("/Shopping/card/:id", card);
+// // function card(request, response) {
+// //   response.send(elements.card);
+// // }
+// app.post("/login", Login);
+// function Login(request, response) {
+//   response.send(elements.login);
+// }
+// app.get("/login", Login);
+// function Login(request, response) {
+//   response.send(elements.login);
+// }
+// app.post("/register", Register);
+// function Register(request, response) {
+//   response.send(elements.register);
+// }
+// app.get("/register", Register);
+// function Register(request, response) {
+//   response.send(elements.register);
+// }
+
+// app.post("/payment", Payment);
+// function Payment(request, response) {
+//   response.send(elements.payment);
+// }
+// app.get("/payment", Payment);
+// function Payment(request, response) {
+//   response.send(elements.payment);
+// }
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+console.log(router);
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
